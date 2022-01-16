e0fc8e733d97fa3e740faede9072c1d1e10abd9a
hoangquydat <dathoang9797@gmail.com>
2022-01-16 12:01:05 +0700

    Fix conflict

--- Studios/js/fixMenu.js
+++ Studios/js/fixMenu.js
@@ -1,17 +1,4 @@
 const fixMenuHeader = () => {
-<<<<<<< HEAD
-    $(window).on("scroll", function () {
-      const scroll = $(window).scrollTop();
-      console.log(scroll)
-      if (scroll >= 80) {
-        $("#header__menu").addClass("header__menu__fixed--top ");
-      } else {
-        $("#header__menu").removeClass("header__menu__fixed--top ");
-      }
-    });
-  };
-  export default fixMenuHeader;
-=======
   $(window).on('scroll', function () {
     const scroll = $(window).scrollTop();
     if (scroll >= 80) {
@@ -22,4 +9,3 @@ const fixMenuHeader = () => {
   });
 };
 export default fixMenuHeader;
->>>>>>> 15d1837 (Delete console scroll)
