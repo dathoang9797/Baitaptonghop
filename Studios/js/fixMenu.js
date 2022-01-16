a5d8eca7e48c2e458c136144e4e46afe72e606b0
hoangquydat <dathoang9797@gmail.com>
2022-01-16 07:04:24 +0700

    Delete console scroll

--- Studios/js/fixMenu.js
+++ Studios/js/fixMenu.js
@@ -1,4 +1,5 @@
 const fixMenuHeader = () => {
+<<<<<<< HEAD
     $(window).on("scroll", function () {
       const scroll = $(window).scrollTop();
       console.log(scroll)
@@ -10,3 +11,15 @@ const fixMenuHeader = () => {
     });
   };
   export default fixMenuHeader;
+=======
+  $(window).on('scroll', function () {
+    const scroll = $(window).scrollTop();
+    if (scroll >= 80) {
+      $('#header__menu').addClass('header__menu__fixed--top ');
+    } else {
+      $('#header__menu').removeClass('header__menu__fixed--top ');
+    }
+  });
+};
+export default fixMenuHeader;
+>>>>>>> 15d1837 (Delete console scroll)
